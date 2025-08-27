de0cb6c4e564ad3786369cf30ed791102e033f5c
onur yucel <onuryucel200@hotmail.com>
2025-08-27 15:39:40 +0300

    v0.22| emu op_LOADi() eklendi

---
+++ Compiler_t2/Source.cpp
@@ -0,0 +1,59 @@
+/*
+
+	LET A = 5
+	LET B = 3
+
+	//A = 3
+	A = B
+
+	//A = 4
+	A = A + 1
+
+	IF A > 0 THEN
+		
+		//4
+		LET C = A
+		//7 = 4 + 3
+		C = C + B
+
+	ENDIF
+
+	WHILE C > 0 
+
+		C = C - 1
+
+	WEND
+
+	//SUM() ?
+	//GOTO SUM() ?
+	GO SUM()
+
+	RETURN
+
+	FUNC SUM()		
+		A = A + 1
+		RETURN
+
+		
+
+
+*/
+
+/*
+--------------
+ 2+5
+ 13+24
+--------------
+ LET A = 5
+--------------
+ A = A + 1
+ A = A - 1
+--------------
+*/
+
+
+
+int main()
+{
+
+}
--- Emu/Source.cpp
+++ Emu/Source.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <cstdint>
+
 
 //sfml3
 class FramSfml
@@ -120,30 +122,132 @@ Keyboard::~Keyboard()
 {
 }
 
+
+#define scpu_RAM_SIZE 1024
+
+
 class Cpu
 {
 public:
 	Cpu();
 	~Cpu();
 
-private:
-	uint32_t pc;
-	uint32_t regs;
-	
+	void run();
 
+	int* ram;
+private:
+	uint32_t m_currentOpcode;
+	uint32_t m_pc;
+	uint32_t m_registerFile[8] = { 0 };
+	uint8_t m_stackPointer;
+	uint8_t m_interrupt;
+	uint8_t m_flagRegister;
+	
+	void op_LOADi();
+	
 };
 
 Cpu::Cpu()
 {
+	m_currentOpcode = 0;
+	m_pc = 0;	
+	m_stackPointer = 0;
+	m_interrupt = 0;
+	m_flagRegister = 0;
+
+	ram = new int[scpu_RAM_SIZE];
 }
 
 Cpu::~Cpu()
 {
+	delete[] ram;
+}
+
+void Cpu::run()
+{
+	m_currentOpcode = ram[0];
+	op_LOADi();
+}
+
+void Cpu::op_LOADi()
+{
+	uint32_t mask = 0x001c0000;
+	uint32_t registerAdr = m_currentOpcode & mask;
+
+	registerAdr = registerAdr >> 18;
+
+	m_pc++;
+	m_registerFile[registerAdr] = ram[m_pc];
+
+	std::cout << "r1["<< m_registerFile[1] << "]\n";
+}
+
+
+
+void print()
+{
+	std::cout << "hey!\n";
 }
 
 //#include "opcodes.inl"
 
+/*
+*  32 bit 8 register
+*  pc 32 bit
+*  stack pointer ?
+*  interrupt reg 1 bit
+*  flag register
+* 
+*  ram[256]
+*  
+*/
+
+#define scpu_BYTE 8
+#define scpu_WORD 16
+#define scpu_DWORD 32
+
+#define scpu_LOADi_r1 0x01048000
+
+
 int main()
 {
+	Cpu cpu;
+
+	cpu.ram[0] = scpu_LOADi_r1;
+	cpu.ram[1] = 25567;
+
+	cpu.run();
+
 	
+
+
+
+	void (*ptr)();
+
+	ptr = &print;
+
+	ptr();	
+
+	//------------------------------//
+	int* iptr;
+
+	int a = 5;
+
+	iptr = &a;
+
+	//------------------------------//
+	void (*funcList[4])();
+
+	funcList[0] = &print;
+	funcList[1] = &print;
+
+	funcList[0]();
+	funcList[1]();
+
+	
+	using FuncPtr = void (*)();
+	
+	FuncPtr funcPtrList[5];
+
+
 }
