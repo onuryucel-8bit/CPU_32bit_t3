3ef6bf2de35139bc132c59e14f752169e17dbd19
onur yucel <onuryucel200@hotmail.com>
2025-10-03 13:26:13 +0300

    v0.67| Emu\ fonksiyon liste guncellemesi

--- Emu/Cpu.cpp
+++ Emu/Cpu.cpp
@@ -25,18 +25,34 @@ Cpu::Cpu(std::vector<uint32_t>& ram)
 	m_opcodeList[0x02].push_back({ asmc_MOD_Adr ,&Cpu::op_STRadr });
 	m_opcodeList[0x02].push_back({ asmc_MOD_RegAdr ,&Cpu::op_STRrx });
 	m_opcodeList[0x02].push_back({ asmc_MOD_Adr_P_Reg ,&Cpu::op_STRadrRx });
+	//MOV
+	m_opcodeList[0x08].push_back({ asmc_MOD_Adr_P_Reg ,&Cpu::op_MOV });
 
 	//ALU
+		//ADD
 	m_opcodeList[0x10].push_back({ asmc_MOD_Rx_Ry ,&Cpu::op_ADDrxry });
 	m_opcodeList[0x10].push_back({ asmc_MOD_Number ,&Cpu::op_ADDrxi });
+		//SUB
 	m_opcodeList[0x11].push_back({ asmc_MOD_Number ,&Cpu::op_SUBrxi });
+		//MULT
+	m_opcodeList[0x12].push_back({ asmc_MOD_Rx_Ry ,&Cpu::op_MULTrxry});
+		//DIV
+	m_opcodeList[0x13].push_back({ asmc_MOD_Rx_Ry ,&Cpu::op_DIVrxry });
 	
 	//STACK
 	m_opcodeList[0x03].push_back({ asmc_MOD_Empty ,&Cpu::op_CALL});
 	m_opcodeList[0x04].push_back({ asmc_MOD_Empty ,&Cpu::op_RET });
 
 	//JUMP
-	m_opcodeList[0x1E].push_back({ asmc_MOD_Empty ,&Cpu::op_JGZ });
+	m_opcodeList[0x1B].push_back({ asmc_MOD_Empty ,&Cpu::op_JMP });
+	m_opcodeList[0x1c].push_back({ asmc_MOD_Empty ,&Cpu::op_JAZ });
+	m_opcodeList[0x1d].push_back({ asmc_MOD_Empty ,&Cpu::op_JLZ });
+	m_opcodeList[0x1e].push_back({ asmc_MOD_Empty ,&Cpu::op_JGZ });
+	m_opcodeList[0x1f].push_back({ asmc_MOD_Empty ,&Cpu::op_JSC });
+	m_opcodeList[0x20].push_back({ asmc_MOD_Empty ,&Cpu::op_JUC });
+	//TODO check cSet name
+	m_opcodeList[0x21].push_back({ asmc_MOD_Empty ,&Cpu::op_JCT });
+	//m_opcodeList[0x22].push_back({ asmc_MOD_Empty ,&Cpu::op_JCF });
 
 }
 
